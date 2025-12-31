30dff9444b32c13c8738a5d4229fac7d835d6abb
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-12-31 09:40:35 +0300

    Update page title and add logo image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
-  title: "GitHub Stats 2025 | Username-Based GitHub Analytics",
+  title: "GitHub Stats 2025",
   description: "Check GitHub 2025 statistics in seconds. Enter any GitHub username and explore yearly coding activity and insights.",
 };
 
